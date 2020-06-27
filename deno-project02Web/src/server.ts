cb43d39e31a886ab313591e4ebe335e7b4b2c12b
Maximiliano Usich <maximilianou@gmail.com>
2020-06-27 15:51:12 -0300

    learn_deno_middleware

--- deno-project02Web/src/server.ts
+++ deno-project02Web/src/server.ts
@@ -1,10 +1,17 @@
-import { Application } from 'https://deno.land/x/oak/mod.ts';
+import { Application, Context } from 'https://deno.land/x/oak/mod.ts';
 
 const port = 4600;
 const app = new Application();
 
-app.use( (ctx) => {
+const logging = async (ctx: Context, next: Function) => {
   console.log(`HTTP ${ctx.request.method} on ${ctx.request.url}`);
+  await next();
+};
+
+app.use(logging);
+
+app.use( (ctx) => {
+  console.log(`returning response..`);
   ctx.response.body = 'Starting Deno Learning Project! ';
 });
 

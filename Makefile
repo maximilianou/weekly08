6b2065dfbd82d79e0a04871b2121ce184980629a
Maximiliano Usich <maximiliano.usich@softvision.com>
2020-07-13 17:35:14 -0300

    deno web, listen, get web

--- Makefile
+++ Makefile
@@ -7,3 +7,5 @@ web02:
 	cd deno-project02Web && deno run --allow-net src/server.ts
 web03: 
 	cd denoweb03 && deno run --allow-net src/server.ts
+web04: 
+	cd denoweb04 && deno run --allow-net src/server.ts
---
+++ denoweb04/src/server.ts
@@ -0,0 +1,20 @@
+import { serve } from 'https://deno.land/std/http/server.ts';
+
+const url = 'http://hn.algolia.com/api/v1/search?query=javascript';
+
+const server = serve({ port: 4620 });
+console.log(`Listening: ${JSON.stringify(server)}  `);
+for await ( const req of server ) {
+
+  const result = await fetch(url).then((result) => result.json());
+
+  const stories = result.hits.map( (hit: any) => ({
+    title: hit.title,
+    url: hit.url,
+    createdAt: hit.created_at_i,
+  }) 
+  );
+
+  req.respond(  { body: JSON.stringify(stories) } );
+
+}
